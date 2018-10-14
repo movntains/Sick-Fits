888680c4bf4957b4812272ed10bf81733d88d112
Rachel <oppermanre17@gmail.com>
2018-10-13 21:44:24 -0400

    Initial commit

---
+++ frontend/components/Page.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+import Meta from '../components/Meta';
+import Header from '../components/Header';
+
+class Page extends Component {
+  render() {
+    return (
+      <div>
+        <Meta />
+        <Header />
+        {this.props.children}
+      </div>
+    );
+  }
+}
+
+export default Page;
