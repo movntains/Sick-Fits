d7a7ff987f9ba81f0d3769220e8d72055bfd48ae
Rachel <oppermanre17@gmail.com>
2018-10-14 20:39:55 -0400

    Add initial styling to Page & Header

--- frontend/components/Header.js
+++ frontend/components/Header.js
@@ -1,16 +1,67 @@
+import Link from 'next/link';
+import styled from 'styled-components';
+
 import Nav from './Nav';
 
+const Logo = styled.h1`
+  position: relative;
+  z-index: 2;
+  margin-left: 2rem;
+  font-size: 4rem;
+  transform: skew(-7deg);
+
+  a {
+    padding: 0.5rem 1rem;
+    background: ${props => props.theme.red};
+    color: white;
+    text-transform: uppercase;
+    text-decoration: none;
+  }
+
+  @media (max-width: 1300px) {
+    margin: 0;
+    text-align: center;
+  }
+`;
+
+const StyledHeader = styled.header`
+  .bar {
+    display: grid;
+    grid-template-columns: auto 1fr;
+    justify-content: space-between;
+    align-items: stretch;
+    border-bottom: 10px solid ${props => props.theme.black};
+  }
+
+  .sub-bar {
+    display: grid;
+    grid-template-columns: 1fr auto;
+    border-bottom: 1px solid ${props => props.theme.lightGrey};
+  }
+
+  @media (max-width: 1300px) {
+    .bar {
+      grid-template-columns: 1fr;
+      justify-content: center;
+    }
+  }
+`;
+
 const Header = () => (
-  <div>
+  <StyledHeader>
     <div className="bar">
-      <a href="">Sick Fits</a>
+      <Logo>
+        <Link href="/">
+          <a>Sick Fits</a>
+        </Link>
+      </Logo>
       <Nav />
     </div>
     <div className="sub-bar">
       <p>Search</p>
     </div>
     <div>Cart</div>
-  </div>
+  </StyledHeader>
 );
 
 export default Header;
