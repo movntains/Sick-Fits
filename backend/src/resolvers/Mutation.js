016a19610937196ab7c334b84a1527f1df684793
Rachel <oppermanre17@gmail.com>
2018-10-15 18:50:16 -0400

    Add query & mutation for items

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -1,3 +1,15 @@
-const Mutations = {};
+const Mutations = {
+  async createItem(parent, args, ctx, info) {
+    // TODO: Check if the user is logged in
+
+    const item = await ctx.db.mutation.createItem({
+      data: {
+        ...args
+      }
+    }, info);
+
+    return item;
+  }
+};
 
 module.exports = Mutations;
