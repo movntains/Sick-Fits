7e9e08dd3c710f0e05df71208d389ea056eb2bc4
Rachel <oppermanre17@gmail.com>
2018-12-01 13:39:37 -0500

    Add user signup functionality to backend

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,3 +1,4 @@
+const cookieParser = require('cookie-parser');
 require('dotenv').config({ path: 'variables.env' });
 
 const createServer = require('./createServer');
@@ -5,14 +6,16 @@ const db = require('./db');
 
 const server = createServer();
 
-// TODO: Use Express middleware to handle cookies (JWT)
+// Use Express middleware to handle cookies (JWT)
+server.express.use(cookieParser());
+
 // TODO: Use Express middleware to populate current user
 
 server.start(
   {
     cors: {
       credentials: true,
-      origin: process.env.FRONTEND_URL,
+      origin: process.env.FRONTEND_URL
     }
   },
   deets => {
--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -1,3 +1,6 @@
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
 const Mutations = {
   async createItem(parent, args, ctx, info) {
     // TODO: Check if the user is logged in
@@ -42,6 +45,37 @@ const Mutations = {
 
     // Delete the item
     return ctx.db.mutation.deleteItem({ where }, info);
+  },
+  async signup(parent, args, ctx, info) {
+    // Set the user's email to lowercase
+    args.email = args.email.toLowerCase();
+
+    // Hash the user's password
+    const password = await bcrypt.hash(args.password, 10);
+
+    // Create the user in the database
+    const user = await ctx.db.mutation.createUser(
+      {
+        data: {
+          ...args,
+          password,
+          permissions: { set: ['USER'] }
+        }
+      },
+      info
+    );
+
+    // Create the JWT for the user
+    const token = jwt.sign({ userID: user.id }, process.env.APP_SECRET);
+
+    // Set the JWT as a cookie on the response
+    ctx.response.cookie('token', token, {
+      httpOnly: true,
+      maxAge: 1000 * 60 * 60 * 24 * 365
+    });
+
+    // Return the user to the browser
+    return user;
   }
 };
 
