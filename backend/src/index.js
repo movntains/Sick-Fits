709ab243c978268634defe58228fd53686c4e512
Rachel <oppermanre17@gmail.com>
2018-12-02 18:21:58 -0500

    Add JWT decoding

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,4 +1,5 @@
 const cookieParser = require('cookie-parser');
+const jwt = require('jsonwebtoken');
 require('dotenv').config({ path: 'variables.env' });
 
 const createServer = require('./createServer');
@@ -9,7 +10,19 @@ const server = createServer();
 // Use Express middleware to handle cookies (JWT)
 server.express.use(cookieParser());
 
-// TODO: Use Express middleware to populate current user
+// Decode the JWT in order to obtain the user ID on each request
+server.express.use((req, res, next) => {
+  const { token } = req.cookies;
+
+  if (token) {
+    const { userId } = jwt.verify(token, process.env.APP_SECRET);
+
+    // Put the user ID onto the request for future requests to access
+    req.userID = userId;
+  }
+
+  next();
+});
 
 server.start(
   {
--- backend/src/resolvers/Query.js
+++ backend/src/resolvers/Query.js
@@ -3,13 +3,20 @@ const { forwardTo } = require('prisma-binding');
 const Query = {
   items: forwardTo('db'),
   item: forwardTo('db'),
-  itemsConnection: forwardTo('db')
+  itemsConnection: forwardTo('db'),
+  me(parent, args, ctx, info) {
+    // Check if there's a current user ID
+    if (!ctx.request.userId) {
+      return null;
+    }
 
-  // async items(parent, args, ctx, info) {
-  //   const items = await ctx.db.query.items();
-
-  //   return items;
-  // }
+    return ctx.db.query.user(
+      {
+        where: { id: ctx.request.userId }
+      },
+      info
+    );
+  }
 };
 
 module.exports = Query;
